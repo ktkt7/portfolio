f68b14d3ae9cb7d261791f31cee7a79274c094a9
ktkt7 <159773236+ktkt7@users.noreply.github.com>
2024-06-01 01:41:06 +0900

    commit

--- js/script.js
+++ js/script.js
@@ -73,7 +73,7 @@
         index++;
         setTimeout(typeMessage, 150); // タイピングの速度を調整
       } else {
-        setTimeout(showMainContent, 1000); // メインコンテンツを表示するまでの遅延を調整
+        setTimeout(showMainContent, 2000); // メインコンテンツを表示するまでの遅延を調整
       }
     }
   
